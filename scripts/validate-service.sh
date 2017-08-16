391043f8aff9987e766e764cdd6e7abb00273f92
felipekm <felipe.kautzmann@gmail.com>
2017-08-16 14:59:51 -0300

    FIX #11

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Removes codedeploy old deploy folder
-sudo rm -rf /opt/codedeploy-agent/deployment-root
-
 # Clean pm2 log files
 sudo pm2 flush
+
+# Removes codedeploy old deploy folder
+sudo rm -rf /opt/codedeploy-agent/deployment-root
 
 # Getting out
 cd ~
